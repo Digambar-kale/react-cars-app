3c73c00652f101123258bb459df4a33f38df48fd
Digambar-kale <digsleo@gmail.com>
2023-01-24 12:08:05 +0530

    first commit

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        data: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+
+        //reducer functions 
+        addCar(state, action) {
+            //Assumption:
+            // action.payload === {name: 'abc, cost: 5000}
+            state.data.push({
+                name: action.payload.name,
+                cost: action.payload.cost,
+                id: nanoid(),
+            });
+        },
+        removeCar(state, action) {
+            // Assumption:
+            // action.payload === the id of the car we want to remove
+            const updated = state.data.filter((car) => {
+                return car.id !== action.payload
+            });
+            state.data = updated;
+        },
+    },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
